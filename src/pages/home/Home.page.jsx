c4b8dd66989ecff5d9982e74e3d94a0b7544faca
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2024-05-20 17:02:00 +0630

    change-headline

--- src/pages/home/Home.page.jsx
+++ src/pages/home/Home.page.jsx
@@ -6,14 +6,15 @@ export default function HomePage() {
     <main className=" ">
       <section className=" container mx-auto min-h-[85vh] flex items-center">
         <div>
-          <h2 className=" text-[36px] text-gray-800 md:text-[64px] font-bold">
-            Digital Diary
+          <h2 className=" text-[30px] text-gray-800 md:text-[54px] font-bold">
+            Digital Diary For Every Moment
           </h2>
           <p className=" text-lg text-gray-700 leading-snug tracking-wide mt-[20px] md:mt-[20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dicta
-            fuga itaque maxime dolorem saepe molestiae minus voluptas! Aperiam
-            blanditiis aliquam numquam facere eveniet aut porro sapiente id quas
-            deleniti
+            Welcome to Digital Diary, where your story takes center stage. Our
+            platform offers a digital sanctuary for your memories, musings, and
+            reflections. Embrace each moment and watch as your journey unfolds,
+            beautifully chronicled in your personalized digital diary. Start
+            documenting your life today with Digital Diary.
           </p>
           <Link to={"/login"}>
             <Button className=" mt-[40px] ">Start Now</Button>
