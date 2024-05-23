642f685275e2fee8f77ae24f421700112fec7aa7
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2024-05-23 16:16:47 +0630

    link-btn-conditional

--- src/pages/home/Home.page.jsx
+++ src/pages/home/Home.page.jsx
@@ -1,7 +1,9 @@
 import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import useAuth from "../../hook/useAuth";
 
 export default function HomePage() {
+  const { currentUser } = useAuth();
   return (
     <main className=" ">
       <section className=" container mx-auto min-h-[85vh] flex items-center">
@@ -16,7 +18,7 @@ export default function HomePage() {
             beautifully chronicled in your personalized digital diary. Start
             documenting your life today with Digital Diary.
           </p>
-          <Link to={"/login"}>
+          <Link to={currentUser?.id ? "/memos" : "/login"}>
             <Button className=" mt-[40px] ">Start Now</Button>
           </Link>
         </div>
